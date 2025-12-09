c50fdf0e1a191a897324ea9fb95e0caf05e7d893
Iurii Nartov <yurahttp@gmail.com>
2025-12-09 22:02:32 +0200

    NodeJS CI: Error: playwright.config.ts(30,5): error TS2769: No overload matches this call.

--- framework/playwright.config.ts
+++ framework/playwright.config.ts
@@ -1,1 +1,37 @@
+import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
 
+const baseURL = process.env.BASE_URL || 'https://book.distribusion.com/?retailerPartnerNumber=807197';
+const artifactsDir = process.env.HOST_ARTIFACTS || 'playwright-report';
+const htmlResultsDir = path.join(artifactsDir, 'test-results');
+
+export default defineConfig({
+  testDir: './tests',
+  outputDir: artifactsDir,
+  navigationTimeout: 30 * 1000,
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 10 * 1000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: [
+    ['line'],
+    ['html', { outputFolder: htmlResultsDir, open: 'never' }],
+  ],
+  use: {
+    baseURL,
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    viewport: { width: 1280, height: 720 },
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
