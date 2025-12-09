aeecaef581c539fcf52550017e09863bf7d5445a
Iurii Nartov <yurahttp@gmail.com>
2025-12-09 23:07:57 +0200

    playwright.config.ts: updates

--- framework/playwright.config.ts
+++ framework/playwright.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   },
   fullyParallel: true,
   forbidOnly: !!process.env.forbidOnly,
-  retries: process.env.retries ? 2 : 1,
-  workers: process.env.workers ? 2 : 4,
+  retries: process.env.retries || 1,
+  workers: process.env.workers || 4,
   reporter: [
     ['line'],
     ['html', { outputFolder: htmlResultsDir, open: 'never' }],
